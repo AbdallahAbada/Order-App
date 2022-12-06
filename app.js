2ba6b760a96c864fa1b594bb8191b61d867c8a84
AbdallahAbada <abdallah.abada.1993@gmail.com>
2022-12-06 00:14:20 +0000

    Adding the custom Event

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 //  SELLECTION
-let orderBtn = document.querySelector(".order-btn");
-let wait = document.getElementById('wating')
+const orderBtn = document.querySelector(".order-btn");
 
 // EVENTS
-orderBtn.addEventListener('click', function resgId() {
-    let userId = prompt("Please enter your ID", "User ID")
+orderBtn.addEventListener('click', () => {
+    userId = prompt("Please enter your ID", "User ID")
     console.log(`Cooking a meal to ${userId}`)
-    let readyMeal = setTimeout(mealReady, 5000)
-    function mealReady() {
-        console.log(`Serving a meal to client ${userId}`)
-        wait.innerHTML = 'Your Meal is ready!!'
-    }
+
+    const event = new CustomEvent("ready", {
+        detail: {
+            user: userId
+        }
+    });
+
+    setTimeout(() => {
+        document.dispatchEvent(event)
+    }, 3000)
+
+})
+
+document.addEventListener("ready", (e) => {
+    console.log(`Serving a meal to client ${e.detail.user}`)
 })
 
